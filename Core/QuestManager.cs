3463ced98e155f029cf27b7ca353375f163d2975
Pablo Bertaco <bertacopablo@gmail.com>
2025-08-20 03:53:50 -0300

    Add quest line registration and auto-progression
    Introduces quest line sequencing via registerQuestLine, enabling parent-child relationships between quests. Adds automatic starting of next quests upon completion, type-based overloads for quest state management, and a method to load all active quests. Refactors quest loading and updating logic to support these features.

--- Core/QuestManager.cs
+++ Core/QuestManager.cs
@@ -4,11 +4,59 @@ public class QuestManager<T> where T : Quest, new()
 {
     Dictionary<string, Quest> dictionary = new();
 
-    public void load<N>(bool start = false) where N : T, new()
-    {
-        N quest = new();
+    Dictionary<Type, Type> parentByType = new();
+    Dictionary<Type, Type> nextByType = new();
+    HashSet<Type> registeredTypes = new();
 
-        quest.started = start || getQuestStarted(quest.id);
+    public QuestManager<T> registerQuestLine(List<Type> sequence)
+    {
+        for (int i = 0; i < sequence.Count; i++)
+        {
+            Type t = sequence[i];
+
+            registeredTypes.Add(t);
+
+            if (i > 0)
+            {
+                Type prev = sequence[i - 1];
+                parentByType[t] = prev;
+                nextByType[prev] = t;
+            }
+        }
+
+        return this;
+    }
+
+    public void start(Type type)
+    {
+        setQuestStarted(type);
+        load(type);
+    }
+
+    public void start<Q>() where Q : T, new() => start(typeof(Q));
+
+    public void load<N>() where N : T, new()
+    {
+        load(typeof(N));
+    }
+
+    public void load(Type type)
+    {
+        string id = type.Name;
+
+        if (dictionary.ContainsKey(id))
+        {
+            return;
+        }
+
+        T quest = (T)Activator.CreateInstance(type)!;
+
+        if (parentByType.TryGetValue(type, out Type parentT))
+        {
+            quest.parentQuest = parentT;
+        }
+
+        quest.started = getQuestStarted(quest.id);
         quest.completed = getQuestCompleted(quest.id);
 
         if (quest.started && !quest.completed)
@@ -19,6 +67,8 @@ public class QuestManager<T> where T : Quest, new()
 
     public void update(Func<T, bool> action)
     {
+        List<Type> nextToAutoStart = new();
+
         foreach (T quest in dictionary.Values)
         {
             if (quest.started && quest.completed)
@@ -28,10 +78,23 @@ public class QuestManager<T> where T : Quest, new()
 
             if (quest.started)
             {
+                if (quest.parentQuest != null && !getQuestCompleted(quest.parentQuest))
+                {
+                    continue;
+                }
+
                 if (action(quest))
                 {
                     quest.completed = true;
                     setQuestCompleted(quest.id);
+
+                    if (nextByType.TryGetValue(quest.GetType(), out Type nextType))
+                    {
+                        if (!getQuestCompleted(nextType) && !getQuestStarted(nextType))
+                        {
+                            nextToAutoStart.Add(nextType);
+                        }
+                    }
                 }
             }
             else
@@ -43,20 +106,41 @@ public class QuestManager<T> where T : Quest, new()
                 }
             }
         }
+
+        foreach (Type t in nextToAutoStart)
+        {
+            setQuestStarted(t);
+            load(t);
+        }
     }
 
     public virtual void setQuestStarted(string id) { }
+    public virtual void setQuestStarted(Type type) => setQuestStarted(type.Name);
 
     public virtual bool getQuestStarted(string id) => false;
+    public virtual bool getQuestStarted(Type type) => getQuestStarted(type.Name);
 
     public virtual void setQuestCompleted(string id) { }
+    public virtual void setQuestCompleted(Type type) => setQuestCompleted(type.Name);
 
     public virtual bool getQuestCompleted(string id) => false;
-}
+    public virtual bool getQuestCompleted(Type type) => getQuestCompleted(type.Name);
 
+    public void loadAll()
+    {
+        foreach (Type t in registeredTypes)
+        {
+            if (getQuestStarted(t) && !getQuestCompleted(t))
+            {
+                load(t);
+            }
+        }
+    }
+}
 
 public class Quest
 {
+    public Type parentQuest;
     public string id { get => GetType().Name; }
     public bool started;
     public bool completed;
