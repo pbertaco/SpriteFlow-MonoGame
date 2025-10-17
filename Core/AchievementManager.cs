3f05067112e76f6a8461cd747cfdb0fccdd539fd
Pablo Bertaco <bertacopablo@gmail.com>
2025-10-17 12:27:21 -0300

    Comment out Steam achievement API calls
    SteamUserStats API calls for getting and setting achievements have been commented out in AchievementManager.cs. This may be for testing, debugging, or to temporarily disable Steam integration.

--- Core/AchievementManager.cs
+++ Core/AchievementManager.cs
@@ -15,7 +15,7 @@ public class AchievementManager<T> where T : Achievement, new()
             try
             {
                 string pchName = achievement.id.ToString();
-                SteamUserStats.GetAchievement(pchName, out unlocked);
+                //SteamUserStats.GetAchievement(pchName, out unlocked);
             }
             catch
             {
@@ -37,8 +37,8 @@ public class AchievementManager<T> where T : Achievement, new()
             try
             {
                 string pchName = achievement.id.ToString();
-                SteamUserStats.SetAchievement(pchName);
-                SteamUserStats.StoreStats();
+                //SteamUserStats.SetAchievement(pchName);
+                //SteamUserStats.StoreStats();
             }
             catch
             {
