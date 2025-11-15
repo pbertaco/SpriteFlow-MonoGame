46ae3799e14d9ba57c43b15dae2753afa8180dcb
Pablo Bertaco <bertacopablo@gmail.com>
2025-11-15 18:57:30 -0300

    Add volume control to DActionPlaySoundEffect
    Introduces static volume property with scaling and clamping for sound effect playback. Updates runOnNode to use the specified volume when playing sound effects.

--- Action/DActionPlaySoundEffect.cs
+++ Action/DActionPlaySoundEffect.cs
@@ -1,7 +1,20 @@
+using SharpDX.Direct3D9;
+
 namespace Dragon;
 
 public class DActionPlaySoundEffect : DAction
 {
+    static float _volume;
+    static float volumeScale = 0.1f;
+    public static float volume
+    {
+        get => _volume / volumeScale;
+        set
+        {
+            _volume = MathHelper.Clamp(value, 0f, 1f) * volumeScale;
+        }
+    }
+
     SoundEffect soundEffect;
 
     public DActionPlaySoundEffect(SoundEffect soundEffect, bool waitForCompletion) : base(0)
@@ -16,7 +29,7 @@ public class DActionPlaySoundEffect : DAction
 
     public override void runOnNode(DNode node)
     {
-        soundEffect?.Play();
+        soundEffect?.Play(volume, 0.0f, 0.0f);
         Console.WriteLine("Playing sound effect" + soundEffect?.Name);
     }
 }
